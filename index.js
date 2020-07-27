8880057c320058e4175c18d3e8fef671e002938a
vkrutsina <vera.krutsina@gmail.com>
2020-07-27 09:26:45 -0700

    entry button addition

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const entryForm = document.getElementById('entryForm');
-const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
-
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
+let count = 1;
 function addEntryToDom(event) {
   event.preventDefault();
   const entryDiv = document.createElement('div');
   entryDiv.className = 'single-entry';
-  entryDiv.innerText = entryTextbox[0].value;
+  entryDiv.innerText = entryTextbox.value;
+  entryDiv.style.display = 'none';
   entriesSection.appendChild(entryDiv);
-  entryTextbox[0].value = '';
+  entryTextbox.value = '';
+
+  const displayEntryButton = document.createElement('button');
+  displayEntryButton.className = 'display-entry-button';
+  displayEntryButton.innerText = count;
+  count++;
+  entriesNav.appendChild(displayEntryButton);
 }
 
 entryForm.addEventListener('submit', addEntryToDom);
